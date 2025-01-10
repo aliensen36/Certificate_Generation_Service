1c38e09e0cec9c40f3413eece24643d52e4ab734
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2025-01-10 21:48:46 +0300

    Настройка докера для data.json

--- certificates/management/commands/export_data.py
+++ certificates/management/commands/export_data.py
@@ -9,8 +9,8 @@ class Command(BaseCommand):
     help = 'Экспортирует данные в файл JSON только для приложения certificates'
 
     def handle(self, *args, **kwargs):
-        project_root = os.path.abspath(os.path.join(os.getcwd(), 'CGS'))
-        file_path = os.path.join(project_root, 'data.json')
+        export_dir = '/usr/src/app/CGS/data'
+        file_path = os.path.join(export_dir, 'data.json')
         output = StringIO()
         call_command('dumpdata', 'certificates', '--indent', '4', stdout=output)
         with open(file_path, 'w', encoding='utf-8') as f:
